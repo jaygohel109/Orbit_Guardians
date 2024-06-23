9ecabec15b069e179f0738e0c362db5dc4171762
Jay Gohel <jaygohel109@gmail.com>
2024-06-23 16:25:11 -0500

    updated chat page  with matching fonts and box style and also fixed logout issue

--- src/components/Navbar/NavDesktop/NavDesktop.js
+++ src/components/Navbar/NavDesktop/NavDesktop.js
@@ -23,46 +23,6 @@ const NavDesktop = ({ pathName, activePlanet, onHover }) => {
                         </Link>
                     </Item>
                 ))}
-                {/* Static Links for Login and Signup */}
-                <Item>
-                    <Link
-                        to="/login"
-                        $bgcolor="#ffffff"
-                        $isActive={pathName === '/login'}
-                        onMouseOver={() => onHover('/login')}
-                        onMouseLeave={() => onHover(false)}
-                        onFocus={() => onHover('/login')}
-                        onBlur={() => onHover(false)}
-                    >
-                        Login
-                    </Link>
-                </Item>
-                <Item>
-                    <Link
-                        to="/signup"
-                        $bgcolor="#ffffff"
-                        $isActive={pathName === '/signup'}
-                        onMouseOver={() => onHover('/signup')}
-                        onMouseLeave={() => onHover(false)}
-                        onFocus={() => onHover('/signup')}
-                        onBlur={() => onHover(false)}
-                    >
-                        Sign Up
-                    </Link>
-                </Item>
-                <Item>
-                    <Link
-                        to="/chat"
-                        $bgcolor="#007bff"
-                        $isActive={pathName === '/chat'}
-                        onMouseOver={() => onHover('/chat')}
-                        onMouseLeave={() => onHover(false)}
-                        onFocus={() => onHover('/chat')}
-                        onBlur={() => onHover(false)}
-                    >
-                        Chat
-                    </Link>
-                </Item>
             </List>
         </Nav>
     );
