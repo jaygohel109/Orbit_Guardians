34111689612828474626e3577107f1641e424428
Jay Gohel <jaygohel109@gmail.com>
2024-06-25 14:26:31 -0500

    added Map in Mars

---
+++ src/components/Map/Map.js
@@ -0,0 +1,38 @@
+// src/components/MapComponent/MapComponent.js
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import './map.css'
+
+// Fix leaflet's default icon issue with webpack
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const MapComponent = () => {
+  const position = [51.505, -0.09]; // Example coordinates, replace with desired position
+
+  return (
+    <MapContainer center={position} zoom={13} className="map-container">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
