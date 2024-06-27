afa93d1f7fa3bc8d4931aa53b27d8e5071d89fb4
VijayBharvad <83714998+vijay0772@users.noreply.github.com>
2024-06-26 22:24:26 -0500

    Initiall commit

---
+++ src/components/Chatbot/api.js
@@ -0,0 +1,9 @@
+// please paste readme.txt code here.
+// then locate this file in terminal and run:    node api.js
+
+
+// before starting the project do not forget to install below dependencies
+
+// 1) npm install express axios cors dotenv
+// 2) npm install @mui/material @emotion/react @emotion/styled @mui/icons-material
+// 3) npm install openai
