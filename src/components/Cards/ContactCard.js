53541f3beb3b251f942392db36c3380951166118
Zeel Thakkar <zeelthakkar25@gmail.com>
2024-06-29 17:52:37 -0400

    Updated the mercury page for responsiveness

--- src/components/Cards/ContactCard.js
+++ src/components/Cards/ContactCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import styled , {keyframes}from 'styled-components';
-
+import styled, { keyframes } from 'styled-components';
 
 const fadeIn = keyframes`
     from {
@@ -14,35 +13,38 @@ const fadeIn = keyframes`
 `;
 
 const CardWrapper = styled.div`
+    background: rgba(240, 240, 240, 0.185);
     color: #fff;
-    width: 95%; /* Adjust width */
-    height: auto; /* Remove fixed height */
-    margin: 20px auto; /* Reduced margin */
+    border-radius: 15px;
+    padding: 15px;
+    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(15px);
+    width: 100%;
+    max-width: 1200px;
+    height: auto;
+    margin: 20px auto;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    font-family: 'Arial', sans-serif;
-    background: rgba(240, 240, 240, 0.185);
-    padding: 3rem;
-    border-radius: 15px;
-    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.3);
-    backdrop-filter: blur(15px);
     text-align: center;
-    animation: fadeIn 1s ease-in-out;
+    animation: ${fadeIn} 1s ease-in-out;
+
+    @media (max-width: 768px) {
+        width: 90%;
+    }
 `;
 
 const CardTitle = styled.h2`
-    font-size: 2.0em; /* Reduced font size */
-    margin-bottom: 15px; /* Reduced margin */
+    font-size: 2.0em;
+    margin-bottom: 15px;
     text-align: center;
-    font-family: 'Orbitron', sans-serif; /* Apply Orbitron font */
+    font-family: 'Orbitron', sans-serif;
 `;
 
 const CardContent = styled.p`
-    font-size: 1em; /* Reduced font size */
+    font-size: 1em;
     text-align: center;
 `;
-
 
 const ContactCard = ({ title, content }) => {
     return (
--- src/components/Cards/WarningCard.js
+++ src/components/Cards/WarningCard.js
@@ -2,40 +2,6 @@ import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { FaExclamationTriangle } from 'react-icons/fa';
 
-const CardWrapper = styled.div`
-    color: #fff;
-    width: 95%; /* Adjust width */
-    height: 500px; /* Set fixed height */
-    margin: 10px auto; /* Reduced margin */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    font-family: 'Arial', sans-serif;
-    background: rgba(240, 240, 240, 0.185);
-    padding: 3rem;
-    border-radius: 15px;
-    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.3);
-    backdrop-filter: blur(15px);
-    text-align: center;
-    margin-top: 20px; /* Adjust margin */
-    animation: fadeIn 1s ease-in-out;
-`;
-
-const CardTitle = styled.h2`
-    font-size: 2.0em; /* Reduced font size */
-    margin-bottom: 15px; /* Reduced margin */
-    text-align: center;
-    font-family: 'Orbitron', sans-serif; /* Apply Orbitron font */
-`;
-
-const CardContent = styled.div`
-    font-size: 1em; /* Reduced font size */
-    flex-grow: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
 const fadeIn = keyframes`
     from {
         opacity: 0;
@@ -58,6 +24,43 @@ const fadeOut = keyframes`
     }
 `;
 
+const CardWrapper = styled.div`
+    background: rgba(240, 240, 240, 0.185);
+    color: #fff;
+    border-radius: 15px;
+    padding: 15px;
+    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.3);
+    backdrop-filter: blur(15px);
+    width: 100%;
+    max-width: 1200px;
+    height: auto;
+    margin: 20px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    text-align: center;
+    animation: ${fadeIn} 1s ease-in-out;
+
+    @media (max-width: 768px) {
+        width: 90%;
+    }
+`;
+
+const CardTitle = styled.h2`
+    font-size: 2.0em;
+    margin-bottom: 15px;
+    text-align: center;
+    font-family: 'Orbitron', sans-serif;
+`;
+
+const CardContent = styled.div`
+    font-size: 1em;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 const ListItem = styled.li`
     animation: ${({ fade }) => (fade === 'in' ? fadeIn : fadeOut)} 1s ease-in-out forwards;
     list-style: none;
@@ -78,8 +81,8 @@ const WarningCard = ({ title, bulletPoints }) => {
             setTimeout(() => {
                 setCurrentIndex((prevIndex) => (prevIndex + 1) % bulletPoints.length);
                 setFade('in');
-            }, 1000); // Match this timeout with the fadeOut animation duration
-        }, 5000); // Duration each message is shown
+            }, 1000);
+        }, 5000);
 
         return () => clearInterval(interval);
     }, [bulletPoints.length]);
