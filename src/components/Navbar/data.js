4d4948790c225a9eeb9c1b61c5783d95fe408f84
Patel, Rishi Sanjaykumar <rsp3@iu.edu>
2024-06-29 23:28:00 -0400

    content

--- src/components/Navbar/data.js
+++ src/components/Navbar/data.js
@@ -6,7 +6,7 @@ export const planets = [
         path: '/mercury',
         color: 'hsl(196, 83%, 93%)',
         sectionColor: 'hsl(194, 48%, 49%)',
-        info: 'Stay updated with the latest news and warnings about alien activities on Mercury.'
+        info: 'Stay updated with the latest news and warnings.'
     },
     {
         id: 1,
@@ -14,7 +14,7 @@ export const planets = [
         path: '/venus',
         color: 'hsl(39, 88%, 73%)',
         sectionColor: 'hsl(33, 82%, 61%)',
-        info: 'Get essential health tips and resources to keep you safe and healthy on Venus.'
+        info: 'Get health tips and resources to stay healthy.'
     },
     {
         id: 2,
@@ -22,7 +22,7 @@ export const planets = [
         path: '/earth',
         color: 'hsl(238, 99%, 66%)',
         sectionColor: 'hsl(263, 67%, 51%)',
-        info: 'Learn about the strengths and weaknesses of different alien species on Earth.'
+        info: 'Learn about the alien species spotted on Hacunia.'
     },
     {
         id: 3,
@@ -30,7 +30,7 @@ export const planets = [
         path: '/mars',
         color: 'hsl(12, 100%, 64%)',
         sectionColor: 'hsl(10, 63%, 51%)',
-        info: 'View the map to see locations where aliens have been spotted on Mars.'
+        info: 'View the locations where aliens have been spotted.'
     },
     {
         id: 4,
@@ -38,7 +38,7 @@ export const planets = [
         path: '/jupiter',
         color: 'hsl(27, 75%, 70%)',
         sectionColor: 'hsl(2, 68%, 53%)',
-        info: 'Chat with fellow citizens and share information about alien threats on Jupiter.'
+        info: 'Chat with fellow citizens about alien threats.'
     },
     {
         id: 5,
@@ -46,7 +46,7 @@ export const planets = [
         path: '/saturn',
         color: 'hsl(40, 96%, 70%)',
         sectionColor: 'hsl(17, 73%, 46%)',
-        info: 'Get trained with our AI survival bot and learn escape strategies on Saturn.'
+        info: 'Chat with our AI survival bot to learn escape strategies.'
     },
     {
         id: 6,
@@ -54,7 +54,7 @@ export const planets = [
         path: '/uranus',
         color: 'hsl(168, 82%, 67%)',
         sectionColor: 'hsl(169, 73%, 44%)',
-        info: 'Stay tuned for exciting updates and features coming soon to Uranus.'
+        info: 'Educational resources for crisis management.'
     },
     {
         id: 7,
@@ -62,6 +62,6 @@ export const planets = [
         path: '/neptune',
         color: 'hsl(222, 95%, 63%)',
         sectionColor: 'hsl(222, 87%, 56%)',
-        info: 'We value your feedback! Share your thoughts and experiences on Neptune.'
+        info: 'We value your feedback!'
     },
 ];
